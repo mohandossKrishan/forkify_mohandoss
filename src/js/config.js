4245b58cd50c95668c33aa02de9239972e1d2e54
mohandoss28@gmail.com <mohandoss28@gmail.com>
2022-05-16 19:50:10 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '23d0a89e-5ad0-4b7c-9e84-d77cb076c3c9';
+export const MODAL_CLOSE_SEC = 2.5;
